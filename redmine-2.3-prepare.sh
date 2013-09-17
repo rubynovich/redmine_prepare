f8eb94ec1a2adfeaf39a0234321677324dd5f0c2
antonovgks <a.antonov@gorkapstroy.ru>
2013-09-17 17:27:49 +0400

    fix: clean up

--- redmine-2.3-prepare.sh
+++ redmine-2.3-prepare.sh
@@ -3,36 +3,41 @@
 # Define common variables
 USERNAME=redmine
 WWW_USERNAME=redmine
-RUN_WITH_USERNAME="sudo -iu $USERNAME http_proxy=$http_proxy https_proxy=$https_proxy"
+RUN_WITH_USERNAME="sudo -iu $USERNAME "
 
 # Create user with $USERNAME
-id $USERNAME || sudo useradd -rm $USERNAME
+id $USERNAME || sudo useradd -gsudo --shell=/bin/bash -rm $USERNAME
 
 # Install apache2
 #sudo apt-get install apache2
 
 # Install RVM and rvm reqirements
-sudo apt-get install curl
+sudo apt-get -y install curl
 curl -L get.rvm.io | $RUN_WITH_USERNAME bash -s stable
 # Install packages suggested by rvm-installer (try 'rvm requirements')
-sudo apt-get install \
+sudo apt-get -y install \
      build-essential openssl libreadline6 libreadline6-dev curl \
      git-core zlib1g zlib1g-dev libssl-dev libyaml-dev \
      libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev \
-     ncurses-dev automake libtool bison subversion pkg-config \
-     mysql-server libmysqlclient-dev libpq-dev zip libcurl4-gnutls-dev \
+     ncurses-dev automake libtool bison subversion pkg-config
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -q -y install mysql-server
+echo "Set MySQL root password later with 'mysqladmin -u root password'"
+
+sudo apt-get -y install \
+     libmysqlclient-dev libpq-dev zip libcurl4-gnutls-dev \
      libmagick++-dev
 
 # Prepare environment for Redmine
 ## Define versions
 REDMINE_VERSION=2.3
-REDMINE_DIR=/opt/redmine-${REDMINE_VERSION}
+REDMINE_DIR=/opt/redmine
 NGINX_DIR=/opt/nginx
-RUBY_VERSION=1.9.3
-GEM_VERSION=1.8.24
+RUBY_VERSION=2.0.0
+GEM_VERSION=2.1.3
 ## Checkout Redmine
 sudo git clone git://github.com/redmine/redmine.git --branch ${REDMINE_VERSION}-stable $REDMINE_DIR
-#sudo svn co http://svn.redmine.org/redmine/branches/${REDMINE_VERSION}-stable $REDMINE_DIR
+# sudo svn co http://svn.redmine.org/redmine/branches/${REDMINE_VERSION}-stable $REDMINE_DIR
 ## Prepare directory and permissions for the bundler
 sudo mkdir -p $REDMINE_DIR/.bundle
 sudo mkdir $NGINX_DIR
@@ -50,8 +55,6 @@ cat << EOF | $RUN_WITH_USERNAME bash -e
     rvm gemset create redmine$REDMINE_VERSION && \
     rvm use $RUBY_VERSION-gems$GEM_VERSION_SHORT@redmine$REDMINE_VERSION
     ## Install bundler (to make a process of dependencies instalation easier)
-    #  aantonov: in next line gem can not follow http redirect
-    #  aantonov: kinda breakpoint
     $GEM_INSTALL bundler
     ## Install passenger
     $GEM_INSTALL passenger
@@ -70,3 +73,4 @@ sudo mkdir -p tmp public/plugin_assets
 sudo chown -R $WWW_USERNAME files log tmp public/plugin_assets db
 sudo chmod -R 755 files log tmp public/plugin_assets
 sudo chmod -R 700 db
+
